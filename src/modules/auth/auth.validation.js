58e5599cdafa07984df8e2cd3efabd7562888829
ElZekoo <zakaria.ahmed6765@gmail.com>
2025-06-25 18:18:08 +0300

    update

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -1,69 +1,68 @@
-
-
 import joi from "joi";
 import { generlaFielsValidation } from "../../middleware/validation/validation.js";
-import {roles, systemRoles} from "../../utils/global/enums.js"
-export const signUpValidation = joi.object({
+import { roles, systemRoles } from "../../utils/global/enums.js";
+export const signUpValidation = joi
+  .object({
     firstName: generlaFielsValidation.firstName.required(),
     lastName: generlaFielsValidation.lastName.required(),
     email: generlaFielsValidation.email.required(),
     password: generlaFielsValidation.password.required(),
-    confirmPassword:generlaFielsValidation.confirmPassword,
+    confirmPassword: generlaFielsValidation.confirmPassword,
     gender: generlaFielsValidation.gender,
     // DOB: joi.date().min('1900-01-01').max('2025-03-02').required(),
     DOB: generlaFielsValidation.DOB,
-    mobileNumber:generlaFielsValidation.mobileNumber,
+    mobileNumber: generlaFielsValidation.mobileNumber,
     systemRole: joi.string().valid(...Object.values(systemRoles)),
     role: joi.string().valid(...Object.values(roles)),
-   learningStyle: joi.string()
-    .when("role", {
+    learningStyle: joi.array().items(joi.string()).when("role", {
       is: "student",
       then: joi.required(),
-      otherwise: joi.optional()
-    })
-    
-}).required();
+      otherwise: joi.optional(),
+    }),
+  })
+  .required();
 
+export const confirmEmail = joi
+  .object({
+    email: generlaFielsValidation.email.required(),
+    OTP: generlaFielsValidation.OTP.required(),
+  })
+  .required();
 
-export const confirmEmail = joi.object(
-    {
-        email: generlaFielsValidation.email.required(),
-        OTP: generlaFielsValidation.OTP.required(),
-
-    }
-).required()
-
-
-export const signInValidation = joi.object({
+export const signInValidation = joi
+  .object({
     email: generlaFielsValidation.email.required(),
     password: generlaFielsValidation.password.required(),
+  })
+  .required();
 
-}).required();
-
-
-export const refreshTokenValidation = joi.object({
+export const refreshTokenValidation = joi
+  .object({
     refreshToken: joi.string().required(),
-}).required()
+  })
+  .required();
 
-
-export const forgetPassword = joi.object({
+export const forgetPassword = joi
+  .object({
     email: generlaFielsValidation.email.required(),
-}).required();
+  })
+  .required();
 
-
-export const resetPasswordValidation = joi.object({
+export const resetPasswordValidation = joi
+  .object({
     email: generlaFielsValidation.email.required(),
     newPassword: generlaFielsValidation.password.required(),
     OTP: generlaFielsValidation.OTP,
-}).required();
+  })
+  .required();
 
-
-export const updatePassword = joi.object({
-    oldPassword:generlaFielsValidation.password.required(),
-    newPassowrd:generlaFielsValidation.password
-    .not(joi.ref("oldPassword"))
-    .required()
-    .messages({"message":"new password is the same as old password"}),
-    cNewPassword:joi.string().valid(joi.ref("newPassowrd")).required()
-  
-}).required()
+export const updatePassword = joi
+  .object({
+    oldPassword: generlaFielsValidation.password.required(),
+    newPassowrd: generlaFielsValidation.password
+      .not(joi.ref("oldPassword"))
+      .required()
+      .messages({ message: "new password is the same as old password" }),
+    cNewPassword: joi.string().valid(joi.ref("newPassowrd")).required(),
+  })
+  .required();
