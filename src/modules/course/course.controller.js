d7463fb73db9d984c23946a33c08b220ee641ac0
ElZekoo <zakaria.ahmed6765@gmail.com>
2025-06-26 15:39:33 +0300

    updated

--- src/modules/course/course.controller.js
+++ src/modules/course/course.controller.js
@@ -1,80 +1,88 @@
-import {Router} from "express";
-import * as courseServices from "./course.service.js"
-import {authentication} from "../../middleware/authentication.js"
+import { Router } from "express";
+import * as courseServices from "./course.service.js";
+import { authentication } from "../../middleware/authentication.js";
 import { authorization } from "../../middleware/authorization.js";
 import { endpoint } from "./end.point.js";
 import { isValidate } from "../../middleware/validation/validation.js";
-import * as courseValidation from "./course.validation.js"
+import * as courseValidation from "./course.validation.js";
 const router = Router();
 
-router.post("/add-course", 
-    authentication,
-    authorization(endpoint.course),
-    isValidate(courseValidation.addCourseValidation),
-    courseServices.addCourse)
+router.post(
+  "/add-course",
+  authentication,
+  authorization(endpoint.course),
+  isValidate(courseValidation.addCourseValidation),
+  courseServices.addCourse
+);
 
-router.delete("/:courseId",
-    authentication,
-    authorization(endpoint.course),
-     courseServices.deleteCourse)
+router.delete(
+  "/:courseId",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.deleteCourse
+);
 
-router.post("/createSyllabus/:courseId",
-     authentication,
-     authorization(endpoint.course),
-     courseServices.createSyllabus)
+router.post(
+  "/createSyllabus/:courseId",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.createSyllabus
+);
 
+router.post(
+  "/savesyllabus/:courseId",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.saveSyllabus
+);
 
-router.post("/savesyllabus/:courseId",
-     authentication,
-     authorization(endpoint.course),
-     courseServices.saveSyllabus)
+router.post(
+  "/createPlan/:courseId",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.createPlan
+);
 
+router.put(
+  "/savePlan/:courseId",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.savePlan
+);
 
+router.get(
+  "/:courseId",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.getCourse
+);
 
-router.post("/createPlan/:courseId",
-       authentication,
-     authorization(endpoint.course),
-     courseServices.createPlan)
+router.get(
+  "/",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.getAllCourse
+);
 
-router.put("/savePlan/:courseId",
-       authentication,
-       authorization(endpoint.course),
-     courseServices.savePlan)
+router.get(
+  "/syllabus/:courseId",
+  authentication,
+  authorization(endpoint.course),
+  courseServices.getSyllabus
+);
 
-router.get("/:courseId",
-       authentication,
-       authorization(endpoint.course),
-       courseServices.getCourse)
+// router.get("/coursePlan/:courseId",
+//      authentication,
+//      authorization(endpoint.course),
+//      courseServices.getCoursePlan)
 
+router.get("/:id", courseServices.getCourse);
 
-router.get("/",
-     authentication,
-     authorization(endpoint.course),
-     courseServices.getAllCourse)
-
-
-router.get("/syllabus/:courseId",
-     authentication,
-     authorization(endpoint.course),
-     courseServices.getSyllabus)
-
-router.get("/coursePlan/:courseId",
-     authentication,
-     authorization(endpoint.course),
-     courseServices.getCoursePlan)
-
-
-
-router.get("/:id", courseServices.getCourse)
-
-
-
-
-
-router.post("/createPlan/savePlan/:courseId", courseServices.savePlan)
-router.post("/createLessonContent/:id", courseServices.createLessonContent)
-router.post("/createLessonContent/saveLesson/:id", courseServices.saveLessonContent)
-
-
+router.post("/createPlan/savePlan/:courseId", courseServices.savePlan);
+router.post("/createLessonContent/:id", courseServices.createLessonContent);
+router.post(
+  "/createLessonContent/saveLesson/:id",
+  courseServices.saveLessonContent
+);
 
 export default router;
